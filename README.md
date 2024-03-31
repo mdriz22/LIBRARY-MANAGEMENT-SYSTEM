7702f12cf33e8dede265416ca33aa2cf1a6d6ef1
mdriz22 <mohamedriyaz5050@gmail.com>
2024-03-31 22:43:07 +0530

    commit

--- README.md
+++ README.md
@@ -3,20 +3,4 @@
 A library management system is software that is designed to manage all the functions of a library. It helps librarian to maintain the database of new books and the books that are borrowed by members along with their due dates.
 
 
-![loading](https://raw.githubusercontent.com/mdriz22/LIBRARY-MANAGEMENT-SYSTEM/main/assets/281754270-31d70921-d5ab-4fd7-907f-61e68e28da49.png)
-
-![login](https://raw.githubusercontent.com/mdriz22/LIBRARY-MANAGEMENT-SYSTEM/main/assets/login.png)
-
-![home](https://raw.githubusercontent.com/mdriz22/LIBRARY-MANAGEMENT-SYSTEM/main/assets/281754418-cdc516a9-17fb-4da6-bc1f-27b704360530.png)
-
-![Add Book](https://raw.githubusercontent.com/mdriz22/LIBRARY-MANAGEMENT-SYSTEM/main/assets/281754503-819581a7-a91e-4650-a50c-04a6eaf7d513.png)
-
-![Student registration](https://raw.githubusercontent.com/mdriz22/LIBRARY-MANAGEMENT-SYSTEM/main/assets/281754553-b6b2d0e1-0187-4f50-a662-4c735ba50d33.png)
-
-![issue](https://raw.githubusercontent.com/mdriz22/LIBRARY-MANAGEMENT-SYSTEM/main/assets/issuebook.png)
-
-![return](https://raw.githubusercontent.com/mdriz22/LIBRARY-MANAGEMENT-SYSTEM/main/assets/281754682-8aef7cf9-15e2-4e6d-aa62-b854e8a43fd7.png)
-
-![logout](https://raw.githubusercontent.com/mdriz22/LIBRARY-MANAGEMENT-SYSTEM/main/assets/281754724-1b224c1e-2ca2-448e-8f87-220a8662245a.png)
-
 
--- src/AddBook.java
+++ src/AddBook.java
@@ -1,225 +1,191 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 
-/**
- *
- * @author Sudhir Kushwaha
- */
-public class AddBook extends javax.swing.JFrame {
 
-    /**
-     * Creates new form AddBook
-     */
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.event.*;
+import java.sql.*;
+import java.util.*;
+
+public class AddBook extends JFrame implements ActionListener{
+
+    private JPanel contentPane;
+    private JTextField t1,t2,t3,t4,t5,t6;
+    private JButton b1,b2;
+    JComboBox comboBox;
+        
+    public static void main(String[] args) {
+	new AddBook().setVisible(true);
+    }
+
+    public void random() {
+        Random rd = new Random();
+	t1.setText("" + rd.nextInt(1000 + 1));
+    }
+
     public AddBook() {
-        initComponents();
+        setBounds(600, 200, 518, 442);
+	contentPane = new JPanel();
+	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	setContentPane(contentPane);
+	contentPane.setLayout(null);
+
+	JLabel l1 = new JLabel("Name");
+	l1.setForeground(new Color(47, 79, 79));
+	l1.setFont(new Font("Tahoma", Font.BOLD, 14));
+	l1.setBounds(73, 84, 90, 22);
+	contentPane.add(l1);
+
+	JLabel l2 = new JLabel("ISBN");
+	l2.setForeground(new Color(47, 79, 79));
+	l2.setFont(new Font("Tahoma", Font.BOLD, 14));
+	l2.setBounds(73, 117, 90, 22);
+	contentPane.add(l2);
+
+	JLabel l3 = new JLabel("Publisher");
+	l3.setForeground(new Color(47, 79, 79));
+	l3.setFont(new Font("Tahoma", Font.BOLD, 14));
+	l3.setBounds(73, 150, 90, 22);
+	contentPane.add(l3);
+
+	JLabel l4 = new JLabel("Price");
+	l4.setForeground(new Color(47, 79, 79));
+	l4.setFont(new Font("Tahoma", Font.BOLD, 14));
+	l4.setBounds(73, 216, 90, 22);
+	contentPane.add(l4);
+
+	JLabel l5 = new JLabel("Pages");
+	l5.setForeground(new Color(47, 79, 79));
+	l5.setFont(new Font("Tahoma", Font.BOLD, 14));
+	l5.setBounds(73, 249, 90, 22);
+	contentPane.add(l5);
+
+	JLabel l6 = new JLabel("Book_id");
+	l6.setForeground(new Color(47, 79, 79));
+	l6.setFont(new Font("Tahoma", Font.BOLD, 14));
+	l6.setBounds(73, 51, 90, 22);
+	contentPane.add(l6);
+
+	JLabel l7 = new JLabel("Edition");
+	l7.setForeground(new Color(47, 79, 79));
+	l7.setFont(new Font("Tahoma", Font.BOLD, 14));
+	l7.setBounds(73, 183, 90, 22);
+	contentPane.add(l7);
+
+	t1 = new JTextField();
+	t1.setForeground(new Color(47, 79, 79));
+	t1.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
+	t1.setBounds(169, 54, 198, 20);
+	contentPane.add(t1);
+	t1.setColumns(10);
+
+	t2 = new JTextField();
+	t2.setForeground(new Color(47, 79, 79));
+	t2.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
+	t2.setColumns(10);
+	t2.setBounds(169, 87, 198, 20);
+	contentPane.add(t2);
+
+	t3 = new JTextField();
+	t3.setForeground(new Color(47, 79, 79));
+	t3.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
+	t3.setColumns(10);
+	t3.setBounds(169, 120, 198, 20);
+	contentPane.add(t3);
+
+	t4 = new JTextField();
+	t4.setForeground(new Color(47, 79, 79));
+	t4.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
+	t4.setColumns(10);
+	t4.setBounds(169, 153, 198, 20);
+	contentPane.add(t4);
+
+        t5 = new JTextField();
+	t5.setForeground(new Color(47, 79, 79));
+	t5.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
+	t5.setColumns(10);
+	t5.setBounds(169, 219, 198, 20);
+	contentPane.add(t5);
+
+	t6 = new JTextField();
+	t6.setForeground(new Color(47, 79, 79));
+	t6.setFont(new Font("Trebuchet MS", Font.BOLD, 14));
+	t6.setColumns(10);
+	t6.setBounds(169, 252, 198, 20);
+	contentPane.add(t6);
+
+	comboBox = new JComboBox();
+	comboBox.setModel(new DefaultComboBoxModel(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" }));
+	comboBox.setBounds(173, 186, 194, 20);
+	contentPane.add(comboBox);
+
+        b1 = new JButton("Add");
+	b1.addActionListener(this);
+	b1.setBorder(new CompoundBorder(new LineBorder(new Color(128, 128, 128)), null));
+        b1.setFont(new Font("Trebuchet MS", Font.BOLD, 15));
+	b1.setBounds(102, 300, 100, 33);
+        b1.setBackground(Color.BLACK);
+        b1.setForeground(Color.WHITE);
+	contentPane.add(b1);
+
+	b2 = new JButton("Back");
+	b2.addActionListener(this);
+	b2.setBorder(new CompoundBorder(new LineBorder(new Color(105, 105, 105)), null));
+	b2.setFont(new Font("Trebuchet MS", Font.BOLD, 15));
+	b2.setBounds(212, 300, 108, 33);
+        b2.setBackground(Color.BLACK);
+        b2.setForeground(Color.WHITE);
+	
+	contentPane.add(b2);
+
+	JPanel panel = new JPanel();
+	panel.setBorder(new TitledBorder(new LineBorder(new Color(138, 43, 226), 2), "Add Books", TitledBorder.LEADING,
+			TitledBorder.TOP, null, new Color(0, 0, 255)));
+	panel.setBounds(10, 29, 398, 344);
+	contentPane.add(panel);
+        
+        panel.setBackground(Color.WHITE);
+        contentPane.setBackground(Color.WHITE);
+	random();
+
     }
-    public void clear(){
-        txtid.setText("");
-        txtname.setText("");
-        txtprice.setText("");
-        txtpublisher.setText("");
-        txtyear.setText("");
-       
+    
+    public void actionPerformed(ActionEvent ae){
+        try{
+            conn con = new conn();
+            if(ae.getSource() == b1){
+                String sql = "insert into book(book_id, name, isbn, publisher, edition, price, pages) values(?, ?, ?, ?, ?, ?, ?)";
+		PreparedStatement st = con.c.prepareStatement(sql);
+                // st.setInt(1, Integer.parseInt(textField.getText()));
+		st.setString(1, t1.getText());
+		st.setString(2, t2.getText());
+		st.setString(3, t3.getText());
+		st.setString(4, t4.getText());
+		st.setString(5, (String) comboBox.getSelectedItem());
+		st.setString(6, t5.getText());
+		st.setString(7, t6.getText());
+
+		int rs = st.executeUpdate();
+		if (rs > 0)
+                    JOptionPane.showMessageDialog(null, "Successfully Added");
+		else
+                    JOptionPane.showMessageDialog(null, "Error");
+                t1.setText("");
+                t2.setText("");
+                t3.setText("");
+                t4.setText("");
+                t5.setText("");
+                t6.setText("");
+		st.close();
+            }
+            if(ae.getSource() == b2){
+                this.setVisible(false);
+		new Home().setVisible(true);
+
+            }
+            con.c.close();
+        }catch(Exception e){
+            
+        }
     }
-
-    /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        jButton6 = new javax.swing.JButton();
-        txtyear = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-        txtprice = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        txtpublisher = new javax.swing.JTextField();
-        txtid = new javax.swing.JTextField();
-        txtname = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setUndecorated(true);
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jButton6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/close icon.png"))); // NOI18N
-        jButton6.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton6ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(1088, 0, 50, 31));
-
-        txtyear.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        getContentPane().add(txtyear, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 570, 320, 36));
-
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel2.setText("Book ID");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 210, 239, 46));
-
-        txtprice.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        getContentPane().add(txtprice, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 470, 320, 40));
-
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel3.setText("Book Name");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 300, 239, 46));
-
-        jButton1.setBackground(new java.awt.Color(204, 0, 0));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(242, 242, 242));
-        jButton1.setText("Save");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 670, 120, 30));
-
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel5.setText("Price");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 470, 239, 46));
-
-        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel6.setText("Publisher");
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 400, 239, 46));
-
-        jLabel7.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel7.setText("Publisher Year");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 560, 239, 46));
-
-        txtpublisher.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        getContentPane().add(txtpublisher, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 390, 320, 40));
-
-        txtid.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        getContentPane().add(txtid, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 210, 320, 40));
-
-        txtname.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        getContentPane().add(txtname, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 300, 320, 40));
-
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(242, 242, 242));
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/isue.jpg"))); // NOI18N
-        jLabel1.setText("Add Book Details");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 50, 230, 53));
-
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/All Page Backgraound.jpg"))); // NOI18N
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1140, 770));
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
-        dispose();
-
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton6ActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        PreparedStatement pst;
-        Connection c=Connect.ConnectToDB();
-        if(txtid.getText().equals("")){
-                JOptionPane.showMessageDialog(rootPane, "Please enter Book ID");
-                txtid.requestFocus();
-        }
-        else if(txtname.getText().equals("")){
-                JOptionPane.showMessageDialog(rootPane, "Please enter Book Name");
-                txtname.requestFocus();
-        }
-        else if(txtpublisher.getText().equals("")){
-                JOptionPane.showMessageDialog(rootPane, "Please enter Publisher");
-                txtpublisher.requestFocus();
-        }
-        else if(txtprice.getText().equals("")){
-                JOptionPane.showMessageDialog(rootPane, "Please enter Book Price");
-                txtprice.requestFocus();
-        }
-        else if(txtyear.getText().equals("")){
-                JOptionPane.showMessageDialog(rootPane, "Please enter publisher Year");
-                txtyear.requestFocus();
-        }
-        else{
-        try {
-            pst=c.prepareStatement("insert into library.book(id,name,publisher,price,year,status)values(?,?,?,?,?,?)");
-            pst.setString(1, txtid.getText());
-            pst.setString(2, txtname.getText());
-            pst.setString(3, txtpublisher.getText());
-            pst.setString(4, txtprice.getText());
-            pst.setString(5, txtyear.getText());
-            pst.setString(6, "NotIssued");
-            pst.executeUpdate();
-            JOptionPane.showMessageDialog(rootPane, "Record Saved","Saved",JOptionPane.INFORMATION_MESSAGE);
-            clear();
-        } catch (SQLException ex) {
-            Logger.getLogger(SignIn.class.getName()).log(Level.SEVERE, null, ex);
-        } 
-            }// TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AddBook.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AddBook.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AddBook.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AddBook.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new AddBook().setVisible(true);
-            }
-        });
-    }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JTextField txtid;
-    private javax.swing.JTextField txtname;
-    private javax.swing.JTextField txtprice;
-    private javax.swing.JTextField txtpublisher;
-    private javax.swing.JTextField txtyear;
-    // End of variables declaration//GEN-END:variables
 }
